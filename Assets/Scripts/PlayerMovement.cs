665f91b29f278a87f63813e3ed0b31d18d691207
Tim <XxWolf9630xX@gmail.com>
2018-01-28 10:55:43 -0600

    Created some organizational folders Added Placeholder Square. Added PlayerMovement Script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody2D playerRigidbody2d;
+
+	void Start() {
+		playerRigidbody2d = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		playerRigidbody2d.AddForce (movement * speed);
+	}
+}
