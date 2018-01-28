513f7e6c8c958453ce3d67fe5bfe0132473aa67d
ajr546 <alexander.james@usask.ca>
2018-01-28 16:11:01 -0600

    Initial Map Generator commit

---
+++ Assets/Scripts/GenerateMap.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateMap : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	Mathf.PerlinNoise(0f,0f);
+}
