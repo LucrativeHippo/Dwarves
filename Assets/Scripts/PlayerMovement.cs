d3d65323b321df1e5972e8e76312b05f72a3efcb
Tim <XxWolf9630xX@gmail.com>
2018-01-28 11:28:08 -0600

    Switched movement to a translate based movement instead of a force based movement. (This was to stop the floating of the player when movement keys were released)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
 	public float speed;
-	private Rigidbody2D playerRigidbody2d;
-
-	void Start() {
-		playerRigidbody2d = GetComponent<Rigidbody2D> ();
-	}
 
 	void FixedUpdate() {
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
-		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
-		playerRigidbody2d.AddForce (movement * speed);
+		if (Input.GetKey(KeyCode.W)) {
+			transform.Translate (Vector2.up * speed);
+		}
+		if (Input.GetKey(KeyCode.A)) {
+			transform.Translate (Vector2.left * speed);
+		}
+		if (Input.GetKey(KeyCode.S)){
+			transform.Translate (Vector2.down * speed);
+		}
+		if(Input.GetKey(KeyCode.D)){
+			transform.Translate (Vector2.right * speed);
+		}
 	}
 }
