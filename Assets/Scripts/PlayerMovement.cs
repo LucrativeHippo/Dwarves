b45c06f914f8e1b00bd1b603cb0e3f72de5f0edf
ajr546 <alexander.james@usask.ca>
2018-01-31 15:30:54 -0600

    Merged develop branch
    Reduced calls to transform

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,17 +7,14 @@ public class PlayerMovement : MonoBehaviour {
 	public float speed;
 
 	void FixedUpdate() {
-		if (Input.GetKey(KeyCode.W)) {
-			transform.Translate (Vector2.up * speed);
-		}
-		if (Input.GetKey(KeyCode.A)) {
-			transform.Translate (Vector2.left * speed);
-		}
-		if (Input.GetKey(KeyCode.S)){
-			transform.Translate (Vector2.down * speed);
-		}
-		if(Input.GetKey(KeyCode.D)){
-			transform.Translate (Vector2.right * speed);
-		}
+		Vector2 mov = new Vector2 (0, 0);
+
+		mov += Input.GetKey (KeyCode.W) ? Vector2.up : Vector2.zero;
+		mov += Input.GetKey (KeyCode.S) ? Vector2.down : Vector2.zero;
+
+		mov += Input.GetKey (KeyCode.A) ? Vector2.left : Vector2.zero;
+		mov += Input.GetKey (KeyCode.D) ? Vector2.right : Vector2.zero;
+
+		transform.Translate (mov * speed);
 	}
 }
