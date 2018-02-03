0bcd503946a32bd59006b812088024dd760b3f02
Tim <XxWolf9630xX@gmail.com>
2018-02-03 10:34:30 -0600

    Fixed rotation for player. Set up so rotation can easily be done for code based movement.

--- Assets/Scripts/actionable/actionManagerPlayerRotation.cs
+++ Assets/Scripts/actionable/actionManagerPlayerRotation.cs
@@ -7,16 +7,73 @@ public class actionManagerPlayerRotation : MonoBehaviour {
 	public Transform player;
 	public float radious = 1f;
 
+	private Vector3 actionboxDirection = new Vector3(0, 10);
+
 	void Start() {
 		player = transform.parent.transform;
 	}
 
-	void Update() {
+	void FixedUpdate() {
 //		Vector3 actionboxToMouse = Camera.main.ScreenToWorldPoint (Input.mousePosition) - player.position;
-		Vector3 actionboxToMouse = player.position;
-		actionboxToMouse.z = 0;
-		transform.position = player.position + (radious * actionboxToMouse.normalized);
+		if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A)) {
+			rotateTopLeft ();
+		}
+		else if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D)) {
+			rotateTopRight ();
+		}
+		else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A)) {
+			rotateBottomLeft ();
+		}
+		else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D)) {
+			rotateBottomRight ();
+		}
+		else if (Input.GetKey(KeyCode.W)) {
+			rotateTop ();
+		}
+		else if (Input.GetKey(KeyCode.A)) {
+			rotateLeft ();
+		}
+		else if (Input.GetKey(KeyCode.S)){
+			rotateDown ();
+		}
+		else if(Input.GetKey(KeyCode.D)){
+			rotateRight ();
+		}
+
+		actionboxDirection.z = 0;
+		transform.position = player.position + (radious * actionboxDirection.normalized);
 	}
 
+	private void rotateTop() {
+		actionboxDirection = new Vector3(0, radious);
+	}
+
+	private void rotateLeft() {
+		actionboxDirection = new Vector3(-radious, 0);
+	}
+
+	private void rotateDown() {
+		actionboxDirection = new Vector3(0, -radious);
+	}
+
+	private void rotateRight() {
+		actionboxDirection = new Vector3(radious, 0);
+	}
+
+	private void rotateTopLeft() {
+		actionboxDirection = new Vector3(-radious, radious);
+	}
+
+	private void rotateTopRight() {
+		actionboxDirection = new Vector3(radious, radious);
+	}
+
+	private void rotateBottomRight() {
+		actionboxDirection = new Vector3(radious, -radious);
+	}
+
+	private void rotateBottomLeft() {
+		actionboxDirection = new Vector3(-radious, -radious);
+	}
 
 }
