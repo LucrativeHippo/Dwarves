ba2322b64538ee43274531aa8669077c5a21b74c
Tim <XxWolf9630xX@gmail.com>
2018-02-02 21:46:07 -0600

    Created actor class which will check that there is something infront of it and then send a message when an action key is pressed. An action script is added to each object to recieve a message from the actor script and do whatever it is supposed to.

---
+++ Assets/Scripts/actionable/actor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class actor : MonoBehaviour {
+
+	private bool actionable;
+	public KeyCode actionKey;
+	private GameObject collidedGameObject;
+	private bool canSend;
+
+	void Start() {
+		canSend = true;
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		actionable = true;
+		collidedGameObject = other.gameObject;
+		Debug.Log ("actionable = true");
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		actionable = false;
+		Debug.Log ("actionable = false");
+	}
+
+	void FixedUpdate() {
+		if(Input.GetKey(actionKey) && actionable && canSend) {
+			Debug.Log ("action Logged.");
+			canSend = false;
+			SendMessage ();
+		}
+	}
+
+	void SendMessage() {
+		collidedGameObject.SendMessage ("recieveMessage");
+		canSend = true;
+	}
+}
