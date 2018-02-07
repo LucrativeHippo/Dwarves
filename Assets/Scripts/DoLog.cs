79e553308ada3688a3859a036b2bc99f60ac334d
SKY-DESKTOP\Skyler <skylersampson@shaw.ca>
2018-02-06 18:24:03 -0600

    Implented custom logging tools, Logs are saved to persistentDataPath, currently stores errors in a separate folder, replaces standard unity logger

---
+++ Assets/Scripts/DoLog.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DoLog : MonoBehaviour {
+	private static string kTAG = "PCGame";
+	public Logger myLogger;
+
+	void Awake()
+	{
+		myLogger = new Logger(new GameLogger());
+		Directory.CreateDirectory (Application.persistentDataPath+"/gameLogs");
+		Directory.CreateDirectory (Application.persistentDataPath+"/gameLogs/errorLogs");
+		myLogger.Log(kTAG, "Log Start: "+ System.DateTime.Now);
+		myLogger.LogError (kTAG, "ErrorLogStart");
+	}
+}
+
+
+public class GameLogger : ILogHandler {
+	private FileStream fileStreamMain, fileStreamError;
+	private StreamWriter streamWriterMain, streamWriterError;
+	private ILogHandler defaultLogHandler = Debug.unityLogger.logHandler;
+
+	public GameLogger(){
+		string filePathMain = Application.persistentDataPath + "/gameLogs/mainLog"+System.DateTime.Now.ToString("yyyy-MM-dd_hh-mm-ss")+".txt";
+		string filePathError = Application.persistentDataPath + "/gameLogs/errorLogs/errLog"+System.DateTime.Now.ToString("yyyy-MM-dd_hh-mm-ss")+".txt";
+
+		fileStreamMain = new FileStream(filePathMain, FileMode.OpenOrCreate, FileAccess.Write);
+		fileStreamError = new FileStream(filePathError, FileMode.OpenOrCreate, FileAccess.Write);
+
+		streamWriterMain = new StreamWriter(fileStreamMain);
+		streamWriterError = new StreamWriter(fileStreamError);
+
+
+		Debug.unityLogger.logHandler = this; //Replaces default debug log handler
+	}
+
+	public void LogFormat(LogType logType, UnityEngine.Object context, string format, params object[] args)
+	{
+		if(logType.Equals(LogType.Error)){
+			streamWriterError.WriteLine (String.Format (format, args));
+			streamWriterError.Flush ();
+		}else{
+			streamWriterMain.WriteLine (String.Format (format, args));
+			streamWriterMain.Flush ();
+		}
+
+		defaultLogHandler.LogFormat (logType, context, format, args);
+	}
+
+	public void LogException(Exception exception, UnityEngine.Object context)
+	{
+		defaultLogHandler.LogException (exception, context);
+	}
+}
+
