1466c70d1075a25ca8741a3cb2840b61a42e08be
yanzhao lu <luyanzhaoyal121@gmail.com>
2018-02-09 18:16:00 -0600

    new files

---
+++ Assets/Scripts/unity pathfinding/unityai.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class unityai : MonoBehaviour
+{
+    public Transform destination;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.GetComponent<NavMeshAgent>().destination = destination.position;
+    }
+}
---
+++ Assets/save and load/savemanager.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class savemanager : MonoBehaviour {
+    private static savemanager instance;
+    public List<saveableobject> saveobjects { get; private set; }
+   
+
+    public static savemanager Instance
+    {
+        get
+        {
+            if(instance==null){
+
+                instance = GameObject.FindObjectOfType<savemanager>();
+
+            }
+            return instance;
+        }
+       
+    }
+
+  
+    // Use this for initialization
+
+    void Awake()
+    {
+        saveobjects = new List<saveableobject>();
+    }
+
+ 
+
+    public  void savef(){
+        PlayerPrefs.SetInt("objectcount",saveobjects.Count);
+
+        for (int i = 0; i < saveobjects.Count;i++){
+            saveobjects[i].savefunction(i);
+
+        }
+
+
+
+         }
+
+
+
+    public void Load(){
+
+        foreach(saveableobject obj in saveobjects){
+
+            if(obj !=null){
+
+                Destroy(obj.gameObject);
+            }
+        }
+        saveobjects.Clear();
+
+
+
+        int objectcount = PlayerPrefs.GetInt("objectcount");
+        for (int i = 0; i< objectcount;i++){
+
+
+            string[] value = PlayerPrefs.GetString(i.ToString()).Split('_');
+            GameObject t = null;
+            switch (value[0]) {
+            
+                case"p1":
+                   t = Instantiate(Resources.Load("p1") as GameObject);
+                    break;
+
+                case "p2":
+                     t = Instantiate(Resources.Load("p2") as GameObject);
+                    break;
+                case "target":
+                     t = Instantiate(Resources.Load("target") as GameObject);
+                    break;
+
+            }
+
+            if(t!=null){
+                t.GetComponent<saveableobject>().load(value);
+
+            }
+
+           // Debug.Log(value);
+            //GameObject t = Instantiate(Resources.Load("target") as GameObject);
+           
+        }
+
+    }
+    public Vector3 stringtovector(string value){
+        //(1, 23, 3)
+        value = value.Trim(new char[] { '(', ')' });
+        //after look like 1, 23, 3
+        value = value.Replace(" ", "");
+        //now 1,23,3
+        string[] pos = value.Split(',');
+        //[0]=1 [1]=23 [2]=3
+        return new Vector3(float.Parse(pos[0]), float.Parse(pos[1]), float.Parse(pos[2]));
+
+    }
+
+
+    public Quaternion stringtoquaternion(string value){
+        return Quaternion.identity;
+
+
+    }
+
+  
+
+	
+}
