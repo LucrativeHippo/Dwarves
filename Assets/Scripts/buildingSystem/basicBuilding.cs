6ce07004e3872d9174243dc37626dbe7bcdd99be
Tim <XxWolf9630xX@gmail.com>
2018-02-14 18:31:45 -0600

    Thought I should probably push most things for building. Still need to get the popup menu for the user to select from to display what I everything.
    Created Testing ResourceManager stuff for creating something the removes resources with building is built.
    Created a script that instantiates a building when interacted with.
    Created a script that goes on Buildings to display how many resources are required to build the building.

---
+++ Assets/Scripts/buildingSystem/basicBuilding.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class basicBuilding : MonoBehaviour {
+
+	private GameObject[] buildingPrefabs;
+
+	private GameObject resourceManager;
+
+	void Start () {
+		// Get Resource Manager;
+		resourceManager = GameObject.Find ("resourceManager");
+
+		// Get all building Prefabs in buildingPrefabs folder.
+		buildingPrefabs = Resources.LoadAll ("Prefabs/buildingPrefabs", typeof(GameObject)).Cast<GameObject> ().ToArray ();
+
+		// Log the names of all buildings.
+		foreach (var t in buildingPrefabs) {
+			Debug.Log ("Building Name: " + t.name);
+		}
+	}
+
+	public void recieveAction () {
+		displayMenu ();
+	}
+
+	private void displayMenu () {
+		// TODO: Display a Popup menu for the user to select the building they wish to build.
+
+		// For testing atm.
+		createBuilding (0);
+	}
+
+	private void createBuilding (int buildingNumber) {
+		if (checkResources (buildingNumber)) {
+			Instantiate (buildingPrefabs [buildingNumber], transform.position, Quaternion.identity);
+
+			resourceManager.GetComponent<testingResources> ().useWood (buildingPrefabs [buildingNumber].GetComponent<resourceCost> ().getWoodCost ());
+			resourceManager.GetComponent<testingResources> ().useStone (buildingPrefabs [buildingNumber].GetComponent<resourceCost> ().getStoneCost ());
+			resourceManager.GetComponent<testingResources> ().useGold (buildingPrefabs [buildingNumber].GetComponent<resourceCost> ().getGoldCost ());
+
+			//		resourceManager.SendMessage ("useWood", buildingPrefabs [buildingNumber].GetComponent<resourceCost> ().getWoodCost ());
+			//		resourceManager.SendMessage ("useStone", buildingPrefabs [buildingNumber].GetComponent<resourceCost> ().getStoneCost ());
+			//		resourceManager.SendMessage ("useGold", buildingPrefabs [buildingNumber].GetComponent<resourceCost> ().getGoldCost ());
+		} else {
+			Debug.Log ("Not Enough Resources to build Building: " + buildingNumber);
+		}
+	}
+
+	private bool checkResources (int buildingNumber) {
+		int currentWood = resourceManager.GetComponent<testingResources> ().getWood ();
+		int currentStone = resourceManager.GetComponent<testingResources> ().getStone ();
+		int currentGold = resourceManager.GetComponent<testingResources> ().getGold ();
+
+		int woodCost = buildingPrefabs [buildingNumber].GetComponent<resourceCost> ().getWoodCost ();
+		int stoneCost = buildingPrefabs [buildingNumber].GetComponent<resourceCost> ().getStoneCost ();
+		int goldCost = buildingPrefabs [buildingNumber].GetComponent<resourceCost> ().getGoldCost ();
+
+		if (woodCost > currentWood || stoneCost > currentStone || goldCost > currentGold) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+}
---
+++ Assets/Scripts/buildingSystem/resourceCost.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class resourceCost : MonoBehaviour {
+
+	public int woodCost = 200;
+	public int stoneCost = 150;
+	public int goldCost = 100;
+
+	public int getWoodCost () {
+		return woodCost;
+	}
+
+	public int getStoneCost () {
+		return stoneCost;
+	}
+
+	public int getGoldCost () {
+		return goldCost;
+	}
+
+}
