184f3b7357ea669e4d1d61d1492cbec2cd73b570
Tim <XxWolf9630xX@gmail.com>
2018-02-22 18:58:12 -0600

    Thought I should push at least some of the stuff I have done/In progress. So it generates a menu based on the npc's in the List. Still setting up the controller a bit and again Im not sure how we want it to look so currently it is just a button again that does nothing.

---
+++ Assets/Scripts/NPC_Manager/UIController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour {
+
+    private GameObject mainUI;
+    private GameObject npcManagerCanvas;
+    private bool menuEnabled;
+
+
+    // Use this for initialization
+    void Start () {
+        npcManagerCanvas = GameObject.Find ("NPCManagerCanvas");
+        npcManagerCanvas = GameObject.Find ("MainUICanvas");
+    }
+
+    private void enableMenu () {
+        mainUI.GetComponent<Canvas> ().enabled = menuEnabled;
+        menuEnabled = !menuEnabled;
+        npcManagerCanvas.GetComponent<Canvas> ().enabled = menuEnabled;
+    }
+
+}
