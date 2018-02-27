2e7f5ef074c61a2e6f798567f9f4a768b36471c9
ajr546 <alexander.james@usask.ca>
2018-02-26 18:54:09 -0600

    Working Scripts

--- Assets/Scripts/Resources/ResourceManager.cs
+++ Assets/Scripts/Resources/ResourceManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ResourceManager : MonoBehaviour {
+	[SerializeField]
 	private int [] resourceList = new int[(int)ResourceTypes.NumberOfTypes];
 
 	private bool meat;
