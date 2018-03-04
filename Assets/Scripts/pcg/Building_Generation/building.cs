dddc147f3ab9bf314fee79be51a31b437a61864b
superiorcamel <willrevell@hotmail.com>
2018-03-04 13:44:14 -0600

    more changes to organization plus basic bulding genreation

---
+++ Assets/Scripts/pcg/Building_Generation/building.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class building : MonoBehaviour {
+    [SerializeField]
+    private GameObject wall;
+    [SerializeField]
+    private GameObject floor;
+    [SerializeField]
+    private GameObject door;
+
+    private int xpos;
+    private int ypos;
+    private int zpos;
+
+    private building_generator bg;
+
+
+	// Use this for initialization
+	void Start () {
+        bg = new building_generator((int)gameObject.transform.position.x, (int)gameObject.transform.position.y, (int)gameObject.transform.position.z, gameObject, wall, door, floor);
+        print(bg.getdoorxlocation());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/pcg/Building_Generation/building_generator.cs
+++ Assets/Scripts/pcg/Building_Generation/building_generator.cs
@@ -2,15 +2,109 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class building_generator : MonoBehaviour {
+public class building_generator {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private static int MAX_WIDTH = 16;
+    private static int MAX_HEIGHT = 16;
+    private static int MIN_WIDTH = 8;
+    private static int MIN_HEIGHT = 8;
+    private static int DEPTH = -40;
+    private bool doorPlaced = false;
+
+
+    //building stats
+    private int width;
+    private int height;
+    private int xlocation;
+    private int zlocation;
+    private int ylocation;
+    private int doorxlocation;
+    private int doorzlocation;
+
+
+    //Getters and Setters
+    public int getxlocation()
+    {
+        return xlocation;
+    }
+
+    public int getylocation()
+    {
+        return ylocation;
+    }
+
+    public int getzlocation()
+    {
+        return zlocation;
+    }
+
+    public int getdoorxlocation()
+    {
+        return doorxlocation;
+    }
+
+    public int getdoorzlocation()
+    {
+        return doorzlocation;
+    }
+
+    public int getwidth()
+    {
+        return width;
+    }
+
+    public int getheight()
+    {
+        return height;
+    }
+    //The basic building construcot function
+    public building_generator(int xpos, int zpos, int ypos, GameObject parentBuilding, GameObject wall, GameObject door, GameObject floor)
+    {
+        width = Random.Range(MIN_WIDTH, MAX_WIDTH);
+        height = Random.Range(MIN_HEIGHT, MAX_HEIGHT);
+
+        xlocation = xpos * MAX_WIDTH;
+        zlocation = zpos * MAX_HEIGHT;
+        ylocation = ypos + DEPTH;
+
+        doorxlocation = width/2;
+        doorzlocation = height-1;
+
+        
+
+        for(int i= 0; i < height; i++)
+        {
+            for(int j = 0; j < width; j++)
+            {
+                if(i ==0 || j == 0 || i == height-1 || j == width-1)
+                {
+                    if(j == height-1 && i == width/2 )
+                    {
+                        GameObject temp = GameObject.Instantiate(door, new Vector3(xlocation + i, ylocation, zlocation + j), Quaternion.identity);
+                        var rot = temp.transform.rotation;
+                        rot.x = 1;
+                        temp.transform.rotation = rot;
+                        doorPlaced = true;
+                    }
+                    else
+                    {
+                        GameObject temp = GameObject.Instantiate(wall, new Vector3(xlocation + i, ylocation, zlocation + j), Quaternion.identity);
+                        var rot = temp.transform.rotation;
+                        rot.x = 1;
+                        temp.transform.rotation = rot;
+                        doorPlaced = true;
+                    }
+                }
+                else
+                {
+                    GameObject temp = GameObject.Instantiate(floor, new Vector3(xlocation + i, ylocation, zlocation + j), Quaternion.identity);
+                    var rot = temp.transform.rotation;
+                    rot.x = 1;
+                    temp.transform.rotation = rot;
+                    doorPlaced = true;
+                }
+            }
+        }
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
