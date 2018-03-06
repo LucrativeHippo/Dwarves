2964f993ae428d21cee93626cd873216b47f791f
Tim <XxWolf9630xX@gmail.com>
2018-03-06 16:40:55 -0600

    Building System Quick Fix. Requires some more work but works atm.

--- Assets/Scripts/buildingSystem/basicBuilding.cs
+++ Assets/Scripts/buildingSystem/basicBuilding.cs
@@ -16,8 +16,9 @@ public class basicBuilding : MonoBehaviour {
 
     public GameObject Button_Template;
 
-    private GameObject buildingMenuUIContent;
+    [SerializeField] private GameObject locationObject;
 
+    [SerializeField] private GameObject buildingMenuUIContent;
 
 
     void Start () {
@@ -34,12 +35,7 @@ public class basicBuilding : MonoBehaviour {
         buildingPrefabsObjects = buildingPrefabs;
 
         // Build Menu.
-        buildBuildingMenu ();
-
-        // Log the names of all buildings.
-        foreach (var building in buildingPrefabs) {
-            Debug.Log ("Building Name: " + building.name);
-        }
+//        buildBuildingMenu ();
     }
 
     public void recieveAction () {
@@ -47,11 +43,6 @@ public class basicBuilding : MonoBehaviour {
     }
 
     private void buildBuildingMenu () {
-        Button testHardCodedButton = GameObject.Find ("BuildButton").GetComponent<Button> ();
-        testHardCodedButton.onClick.AddListener (() => createBuilding (0));
-
-
-        // TODO: Finish below for creating buttons to build stuff.
         GameObject tempGameObject;
         int counter = 0;
         foreach (var theBuildings in buildingPrefabs) {
@@ -61,13 +52,18 @@ public class basicBuilding : MonoBehaviour {
             aButtonScript.setBuildingScript (this);
             aButtonScript.setName (theBuildings.name);
             aButtonScript.setNumber (counter);
-            tempGameObject.transform.SetParent (Button_Template.transform.parent, false);
+            tempGameObject.transform.SetParent (buildingMenuUIContent.transform, false);
             counter++;
         }
     }
 
     private void displayMenu () {
+        foreach (Transform child in buildingMenuUIContent.transform) {
+            Destroy (child.gameObject);
+        }
+        buildBuildingMenu ();
         buildingMenu.GetComponent<Canvas> ().enabled = true;
+        locationObject = this.gameObject;
     }
 
 
