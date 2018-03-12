cb964e9e3560948d4eeb0d0b0a74f118c118c69c
Michael Ruffell <mar492@mail.usask.ca>
2018-03-12 15:17:31 -0600

    Added initial player skills script

---
+++ Assets/Scripts/PlayerSkills/PlayerSkills.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSkills : MonoBehaviour {
+
+    public GameObject campfire;
+    public GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.Find("player").gameObject.GetComponentInChildren<actor>().gameObject;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (player != null)
+        {
+            if (Input.GetKey(KeyCode.V))
+            {
+                Debug.Log("Pressed skill");
+                if (campfire != null)
+                {
+                    Instantiate(campfire, player.transform.position, Quaternion.identity);
+                }
+
+            }
+        }
+        
+    }
+}
