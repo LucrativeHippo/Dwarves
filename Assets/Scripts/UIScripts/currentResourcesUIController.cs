c0dadc1a5212ded49c526fe6d738c3a4812ce217
ajr546 <alexander.james@usask.ca>
2018-03-12 11:32:10 -0600

    Attached Res UI

--- Assets/Scripts/UIScripts/currentResourcesUIController.cs
+++ Assets/Scripts/UIScripts/currentResourcesUIController.cs
@@ -9,12 +9,33 @@ public class currentResourcesUIController : MonoBehaviour {
     [SerializeField] private int updateTimer;
 
     private Text goldText;
+    private bool idle = true;
 
     void Start () {
-		
+        updateHUD();
+    }
+
+    IEnumerator wait(){
+        idle = false;
+        
+        yield return new WaitForSeconds (updateTimer);
+        updateHUD();
+        idle = true;
+    }
+
+    private void updateHUD(){
+        for(int i=0;i<(int)ResourceTypes.NumberOfTypes;i++){
+            GameObject g = GameObject.Find(((ResourceTypes)i).ToString().ToLower()+"Text");
+            if(g != null){
+                g.GetComponent<Text>().text = MetaScript.getRes().getResource(i).ToString();
+            }else{
+                Debug.Log("HEY!! "+((ResourceTypes)i).ToString().ToLower());
+            }
+        }
     }
 
     void Update () {
-        
+        if(idle)
+            StartCoroutine(wait());
     }
 }
