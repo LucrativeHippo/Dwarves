9a2abcc906ea9a1eb2effd8d5e80acfccecebc4f
Tim <XxWolf9630xX@gmail.com>
2018-03-12 15:48:43 -0600

    Finished the Health Bar code (works as long as player has a health script)

--- Assets/Scripts/UIScripts/healthBarUIController.cs
+++ Assets/Scripts/UIScripts/healthBarUIController.cs
@@ -1,16 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class healthBarUIController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField] int updateTimer;
+
+    private GameObject player;
+
+    private Text healthNumberText;
+    private GameObject healthBar;
+
+    private IEnumerator coroutine;
+
+    void Start () {
+        player = GameObject.FindWithTag ("Player");
+        healthBar = this.transform.GetChild (1).gameObject;
+        healthNumberText = this.transform.GetChild (2).gameObject.GetComponent<Text> ();
+
+        coroutine = WaitAndUpdate (updateTimer);
+        StartCoroutine (coroutine);
+    }
+
+    private void updateHealth () {
+        float temp = player.GetComponent<Health> ().health;
+        healthNumberText.text = System.Math.Round (temp, 0).ToString () + "%";
+
+        float fractionHealth = temp / 100;
+
+        healthBar.transform.localScale = new Vector3 (fractionHealth, 1.0F, 1.0F);
+    }
+
+    private IEnumerator WaitAndUpdate (float waitTime) {
+        while (true) {
+            yield return new WaitForSeconds (waitTime);
+            updateHealth ();
+        }
+    }
 }
