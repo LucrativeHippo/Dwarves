e88ed8c8467a8240ecb0ed55339823667e3ed10d
Tim <XxWolf9630xX@gmail.com>
2018-03-11 19:22:42 -0600

    Other than stats and image npc manager UI is finished.

---
+++ Assets/Scripts/UIScripts/currentResourcesUIController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class currentResourcesUIController : MonoBehaviour {
+
+    private GameObject resourceManager;
+    [SerializeField] private int updateTimer;
+
+    private Text goldText;
+
+    void Start () {
+		
+    }
+
+    void Update () {
+        
+    }
+}
