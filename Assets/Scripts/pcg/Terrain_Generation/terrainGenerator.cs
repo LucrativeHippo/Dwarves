b2355f94e6bd8badbbf4091ab904b2f0c3a806a9
Michael Ruffell <mar492@mail.usask.ca>
2018-03-12 21:17:18 -0600

    Hopefully working scene.

--- Assets/Scripts/pcg/Terrain_Generation/terrainGenerator.cs
+++ Assets/Scripts/pcg/Terrain_Generation/terrainGenerator.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [DefaultExecutionOrder(-300)]
 public class terrainGenerator : MonoBehaviour
 {
-
+    public GameObject[] lol;
     //The map that shows the terrain value at each existing coordinate
     public Dictionary<string, terrain> terrainMap;
     public Dictionary<string, resource> resourceMap;
@@ -440,6 +440,8 @@ public class terrainGenerator : MonoBehaviour
                 //}
             }
         }
+        lol = GameObject.FindGameObjectsWithTag("TownCenter");
+
         if (GameObject.FindGameObjectWithTag("TownCenter")!=null)
         {
             GameObject.FindGameObjectWithTag("TownCenter").GetComponent<NavMeshBuildFunction>().build();
@@ -447,10 +449,6 @@ public class terrainGenerator : MonoBehaviour
         
         
 
-    }
-    void Start()
-    {
-        //GameObject.FindGameObjectWithTag("TownCenter").GetComponent<NavMeshBuildFunction>().build();
     }
 
     /// <summary>
