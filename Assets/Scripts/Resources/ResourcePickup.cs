5420ab911bdb34dc70590d77df4c215a6bfd0228
superiorcamel <willrevell@hotmail.com>
2018-03-18 15:45:19 -0600

    Added a gold pickup resource for caves and implemented a town portal mechanic

---
+++ Assets/Scripts/Resources/ResourcePickup.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourcePickup : MonoBehaviour, IActionable {
+
+    [NamedArray(typeof(ResourceTypes))]
+    public int[] amount = new int[(int)ResourceTypes.NumberOfTypes];
+
+
+    public void recieveAction()
+    {
+        for (int i = 0; i < amount.Length; i++)
+        {
+            MetaScript.getRes().addResource(i, amount[i]);
+        }
+        Destroy(gameObject);
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
