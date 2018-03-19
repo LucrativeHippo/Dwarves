c4449e8a3c36ce06edabc2cb403ee419a94af9bb
Tim <XxWolf9630xX@gmail.com>
2018-03-19 14:46:56 -0600

    Almost Finished Upgrade Prompt.

--- Assets/Scripts/buildingSystem/upgradeBuilding.cs
+++ Assets/Scripts/buildingSystem/upgradeBuilding.cs
@@ -1,49 +1,72 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
-public class upgradeBuilding : MonoBehaviour, IActionable
-{
+public class upgradeBuilding : MonoBehaviour, IActionable {
     [SerializeField]
-    [NamedArray(typeof(ResourceTypes))]private int[] upgradeCostList = new int[(int)ResourceTypes.NumberOfTypes];
+    [NamedArray (typeof(ResourceTypes))]private int[] upgradeCostList = new int[(int)ResourceTypes.NumberOfTypes];
 
     [SerializeField]
     private GameObject upgrade;
 
     private bool canUpgrade = true;
 
-    public void recieveAction()
-    {
-        for(int i=0; i < (int)ResourceTypes.NumberOfTypes; i++)
-        {
-            if (!MetaScript.getRes().hasResource(i,upgradeCostList[i]))
-            {
+    private GameObject AllUIObjects;
+    private GameObject upgradePrompt;
+
+    private GameObject confirmButton;
+    private GameObject cancelButton;
+
+    private Text nameText;
+    private Text costText;
+    private Text moreResourcesRequiredText;
+
+    public void recieveAction () {
+        AllUIObjects.transform.GetChild (2).gameObject.SetActive (true);
+        upgradePrompt.SetActive (true);
+        nameText.text = upgrade.name.ToString ();
+        string tempCostString = "";
+        for (int i = 0; i < (int)ResourceTypes.NumberOfTypes; i++) {
+            if (!MetaScript.getRes ().hasResource (i, upgradeCostList [i])) {
                 canUpgrade = false;
             }
+            tempCostString += upgradeCostList [i].ToString () + "\n";
         }
+        costText.text = tempCostString;
+        moreResourcesRequiredText.text = "";
+        setListener ();
+    }
 
-        if (canUpgrade)
-        {
-            Instantiate(upgrade, gameObject.transform.parent.transform.position, Quaternion.identity);
-            Destroy(gameObject.transform.parent.gameObject);
+    private void doUpgrade () {
+        if (canUpgrade) {
+            Instantiate (upgrade, gameObject.transform.parent.transform.position, Quaternion.identity);
+            Destroy (gameObject.transform.parent.gameObject);
+            closePrompt ();
+        } else {
+            moreResourcesRequiredText.text = "More Resources Required.";
         }
-        
     }
 
-    void Start()
-    {
-
+    void Start () {
+        AllUIObjects = GameObject.Find ("AllUIObjectsCanvas");
+        upgradePrompt = AllUIObjects.transform.GetChild (2).GetChild (1).gameObject;
+        confirmButton = upgradePrompt.transform.GetChild (0).gameObject;
+        cancelButton = upgradePrompt.transform.GetChild (1).gameObject;
+        costText = upgradePrompt.transform.GetChild (2).gameObject.GetComponent<Text> ();
+        nameText = upgradePrompt.transform.GetChild (3).gameObject.GetComponent<Text> ();
+        moreResourcesRequiredText = upgradePrompt.transform.GetChild (4).gameObject.GetComponent<Text> ();
     }
 
-
-
-    void Update()
-    {
-
+    private void setListener () {
+        confirmButton.GetComponent<Button> ().onClick.AddListener (() => doUpgrade ());
+        cancelButton.GetComponent<Button> ().onClick.AddListener (() => closePrompt ());
     }
 
-
-
+    private void closePrompt () {
+        upgradePrompt.SetActive (false);
+        AllUIObjects.transform.GetChild (2).gameObject.SetActive (false);
+    }
 }
 
