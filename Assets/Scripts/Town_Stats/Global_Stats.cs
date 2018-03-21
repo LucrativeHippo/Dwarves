2396d0240b2fef13f0ea99bf883c314b7f5251b7
superiorcamel <willrevell@hotmail.com>
2018-03-21 13:47:34 -0600

    Added some sprites fixed the prefabs changed foodSystem to use floats and added the global stats script

---
+++ Assets/Scripts/Town_Stats/Global_Stats.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Global_Stats : MonoBehaviour {
+    [SerializeField]
+    private float foodSaved = 0;
+
+    [SerializeField]
+    private float militaryAbility = 1;
+
+    [SerializeField]
+    private float healthPool;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
