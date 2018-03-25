629c23b550fd9c1a9040aa5d7be6e740e9d1fe63
Tim <XxWolf9630xX@gmail.com>
2018-03-24 18:55:24 -0600

    Added a Health bar to NPC's. Also Removed some excess stuff from healthBarUIController.

--- Assets/Scripts/UIScripts/healthBarUIController.cs
+++ Assets/Scripts/UIScripts/healthBarUIController.cs
@@ -3,55 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class healthBarUIController : MonoBehaviour, IHealthListener{
-
-    [SerializeField] int updateTimer;
-
+public class healthBarUIController : MonoBehaviour, IHealthListener {
 
     private Text healthNumberText;
     private GameObject healthBar;
 
-    private IEnumerator coroutine;
-
-    public void publish()
-    {
-        if(playerHealth != null)
-            updateHealth();
+    public void publish () {
+        if (playerHealth != null)
+            updateHealth ();
     }
 
     private Health playerHealth;
-    public void setHealth(Health h)
-    {
-        if(h != null){
+
+    public void setHealth (Health h) {
+        if (h != null) {
             playerHealth = h;
-            h.addSubscriber(this);
+            h.addSubscriber (this);
         }
     }
 
     void Start () {
-        setHealth(MetaScript.getPlayer().GetComponent<Health>());
+        setHealth (MetaScript.getPlayer ().GetComponent<Health> ());
         healthBar = this.transform.GetChild (1).gameObject;
         healthNumberText = this.transform.GetChild (2).gameObject.GetComponent<Text> ();
-
-        // coroutine = WaitAndUpdate (updateTimer);
-        // StartCoroutine (coroutine);
     }
 
     private void updateHealth () {
-        float temp = playerHealth.getHealth();
-        float fractionHealth = temp / (float)playerHealth.getMaxHealth();
+        float fractionHealth = playerHealth.getHealth () / (float)playerHealth.getMaxHealth ();
 
-        healthNumberText.text = System.Math.Round (fractionHealth*100, 0).ToString () + "%";
-
-
+        healthNumberText.text = System.Math.Round (fractionHealth * 100, 0).ToString () + "%";
         healthBar.transform.localScale = new Vector3 (fractionHealth, 1.0F, 1.0F);
-    }
-
-    private IEnumerator WaitAndUpdate (float waitTime) {
-        while (true) {
-            yield return new WaitForSeconds (waitTime);
-            //TODO: make sure the player has health.
-//            updateHealth ();
-        }
     }
 }
