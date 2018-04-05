a3800dd05f4f8639200ba768c56eaf1f2aa128ff
superiorcamel <willrevell@hotmail.com>
2018-04-04 18:40:17 -0600

    more fixes

--- Assets/endGameDialogue.cs
+++ Assets/endGameDialogue.cs
@@ -6,14 +6,16 @@ public class endGameDialogue : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        RPGTalk rpgtalk = gameObject.GetComponent<RPGTalk>();
-
-        rpgtalk.NewTalk("endgame-default-talk-start", "endgame-default-talk-end");
-
+        
     }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void endGame()
+    {
+        Debug.Log("sdvsggsgsggsgsgsgsdggdsggssgdsgdgsdgssgd");
+    }
 }
